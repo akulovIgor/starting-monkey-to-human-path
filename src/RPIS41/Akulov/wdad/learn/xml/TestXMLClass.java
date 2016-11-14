ab1ff10e55d7b382d5877aa050aee9d9828c1e72
akulovIgor <gus.akulov@gmail.com>
2016-11-14 17:46:29 +0400

    Update TestXMLClass.java

--- src/RPIS41/Akulov/wdad/learn/xml/TestXMLClass.java
+++ src/RPIS41/Akulov/wdad/learn/xml/TestXMLClass.java
@@ -2,7 +2,7 @@ package RPIS41.Akulov.wdad.learn.xml;
 
 import org.xml.sax.SAXException;
 
-import javax.security.sasl.SaslException;
+
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.Calendar;
@@ -10,15 +10,15 @@ import java.util.Calendar;
 public class TestXMLClass {
     public static void test(){
         Calendar calendarEarnings = Calendar.getInstance();
-        calendarEarnings.set(2016, Calendar.JANUARY, 8);//дата для проверки прибыли за день
+        calendarEarnings.set(2016, Calendar.OCTOBER, 10);//дата для проверки прибыли за день
         Calendar calendarRemove = Calendar.getInstance();
         calendarRemove.set(2015, Calendar.APRIL, 25);//дата для удаления
 
         try {
-            XmlTask testClass = new XmlTask("src\\RPIS41\\Olukhov\\wdad\\learn\\xml\\testRightFirst");
-            System.out.println("Прибыль Сидорова за 8 января 2016 года: " + testClass.earningsTotal("sidorov", calendarEarnings));
+            XmlTask testClass = new XmlTask("src\\RPIS41\\Akulov\\wdad\\learn\\xml\\testRightFirst");
+            System.out.println("Прибыль Сидорова за 10 октября 2016 года: " + testClass.earningsTotal("sidorov", calendarEarnings));
             testClass.removeDay(calendarRemove);
-            testClass.changeOfficiantName("alexander", "petrov", "sashka", "ivanov");
+            testClass.changeOfficiantName("maxim", "petrov", "vlad", "ivanov");
         }catch (ParserConfigurationException | SAXException | IOException e){e.printStackTrace();}
     }
 }
